f71586be97c286879e5d9b83f75c8fa613ebd607
Michael Anokye <michaelanokyej@yahoo.com>
2020-03-21 22:19:13 -0400

    fixed connections

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
 export default {
-    API_ENDPOINT: 'http://localhost:8000',
+    API_ENDPOINT: 'https://glacial-ridge-46372.herokuapp.com',
     TOKEN_KEY: 'b0a67ced-e993-4b9a-85f4-7ba2425eec64',
 }
 
